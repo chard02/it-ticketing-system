84d281349e50ff1aa536f626407d444749791f8f
Powl <paulusaritonang002@gmail.com>
2026-09-01 10:13:40 +0700

    andra x richard

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -46,7 +46,7 @@ class LoginController extends Controller
 
         return back()
             ->withErrors([
-                'username' => 'Username atau fgjkgmhnbhfh password salah.',
+                'username' => 'Username atau  password salah.',
             ])
             ->onlyInput('username');
     }
