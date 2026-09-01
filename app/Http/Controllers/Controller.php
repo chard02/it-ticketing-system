24c1e4c89a7c8d7fb6b6abe870f7e221832ce5ac
iamryantarigan <ryanwtarigan@gmail.com>
2026-09-01 09:49:58 +0700

    Ryan Tarigan

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -4,5 +4,10 @@ namespace App\Http\Controllers;
 
 abstract class Controller
 {
-    //
+    protected $ticket;
+
+    public function __construct($ticket)
+    {
+        $this->ticket = $ticket;
+    }
 }
