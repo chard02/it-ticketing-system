cf192af3ef53676f17b714961065232ad6e3fdc4
Powl <paulusaritonang002@gmail.com>
2026-09-01 09:54:01 +0700

    richard x rian

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -4,10 +4,5 @@ namespace App\Http\Controllers;
 
 abstract class Controller
 {
-    protected $ticket;
-
-    public function __construct($ticket)
-    {
-        $this->ticket = $ticket;
-    }
+    //
 }
